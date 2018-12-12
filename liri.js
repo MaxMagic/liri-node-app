8c9e07e1b5c06e98197239780976502536e87138
MaxMagic <Lennox.E.David@gmail.com>
2018-12-12 12:20:25 -0500

    update final option

--- liri.js
+++ liri.js
@@ -104,13 +104,81 @@ var liri = {
             if (error){
                 return console.log(error);
             };
+        
             var myData = data.split(',');
-            console.log(myData);
-            operation = myData[0];
-            var title = myData[1];
-
-
-            liri[operation](title);
+    
+            var action = myData[0];
+            var searchItem = myData[1];
+        
+            switch (action){
+                case 'concert-this':
+                    axios.get(`https://rest.bandsintown.com/artists/${searchItem}/events?app_id=${process.env.BANDS_ID}`).then(function( response, error) {
+                        if (error) {
+                            return console.log('Error occurred: ' + error);
+                        }
+                        // Intro
+                        console.log(`The next ${searchItem} concert will be located at:`)
+            
+                        // Name of venue
+                        console.log(`${response.data[0].venue.name}`);
+            
+                        // Venue Location
+                        console.log(`In ${response.data[0].venue.city}, ${response.data[0].venue.country}`);
+            
+                        // Date of event
+                        console.log(`On ${response.data[0].datetime}`);
+                    });
+                    break;
+                case 'spotify-this-song':
+                    spotify.search({ type: 'track', query: searchItem, limit: 1 }, function(err, data) {
+                        if (err) {
+                            return console.log(err);
+                        } 
+                        // Artist
+                        console.log(`Artist: ${data.tracks.items[0].album.artists[0].name}`);
+            
+                        // Song Name
+                        console.log(`Song: ${data.tracks.items[0].name}`);
+            
+                        // Preview Link
+                        console.log(`Preview Link: ${data.tracks.items[0].external_urls.spotify}`);
+            
+                        //Album
+                        console.log(`Album: ${data.tracks.items[0].album.name}`);    
+                    });
+                    break;
+                case 'movie-this':
+                    axios.get(`http://www.omdbapi.com/?apikey=${process.env.OMDB_KEY}&t=${searchItem}`).then(function(response, error) {
+                        if (error) {
+                            return console.log('Error occurred: ' + error);
+                        }
+                        // Movie Title
+                        console.log(`Movie: ${response.data.Title}`);
+            
+                        // Release Year
+                        console.log(`Released: ${response.data.Year}`);
+            
+                        // IMDB Rating
+                        console.log(`IMDB Rating: ${response.data.imdbRating}`);
+            
+                        // Rotten Tomatoes
+                        console.log(`Rotten Tomatoes Score: ${response.data.Ratings[1].Value}`);
+            
+                        // Country of Production
+                        console.log(`Country of Origin: ${response.data.Country}`);
+            
+                        // Movie Language
+                        console.log(`Languages: ${response.data.Language}`);
+            
+                        // Plot
+                        console.log(`Plot: ${response.data.Plot}`);
+            
+                        // Actors
+                        console.log(`Actors: ${response.data.Actors}`);
+                    });
+                    break;
+                    
+            }
         });
     }
 };
--- test3.js
+++ test3.js
@@ -27,63 +27,164 @@
 
 var axios = require('axios');
 
-// axios.get(`https://rest.bandsintown.com/artists/DVSN/events?app_id=0be905f2241374a45f8824e2ff55e3e7`).then(function( response, error) {
+// // axios.get(`https://rest.bandsintown.com/artists/DVSN/events?app_id=0be905f2241374a45f8824e2ff55e3e7`).then(function( response, error) {
+// //         if (error) {
+// //             return console.log('Error occurred: ' + error);
+// //         }
+// //         // Need to display: Name of the venue, Venue location, Date of the Event (use moment to format this as "MM/DD/YYYY")
+// //         // console.log(response.data[0]);
+
+// //         // Name of venue
+// //         console.log(response.data[0].venue.name);
+
+// //         // Venue Location
+// //         console.log(response.data[0].venue.city);
+// //         console.log(response.data[0].venue.country);
+
+// //         // Date of event
+// //         console.log(response.data[0].datetime);
+// //     });
+
+//     axios.get(`http://www.omdbapi.com/?apikey=7aca9df7&t=batman+begins`).then(function(response, error) {
 //         if (error) {
 //             return console.log('Error occurred: ' + error);
 //         }
-//         // Need to display: Name of the venue, Venue location, Date of the Event (use moment to format this as "MM/DD/YYYY")
-//         // console.log(response.data[0]);
+//         /*  * Title of the movie.
+//             * Year the movie came out.
+//             * IMDB Rating of the movie.
+//             * Rotten Tomatoes Rating of the movie.
+//             * Country where the movie was produced.
+//             * Language of the movie.
+//             * Plot of the movie.
+//             * Actors in the movie.
+//             * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+//         */
+//         // console.log(response.data);
 
-//         // Name of venue
-//         console.log(response.data[0].venue.name);
+//         // Movie Title
+//         console.log(`Movie: ${response.data.Title}`);
 
-//         // Venue Location
-//         console.log(response.data[0].venue.city);
-//         console.log(response.data[0].venue.country);
+//         // Release Year
+//         console.log(`Released: ${response.data.Year}`);
 
-//         // Date of event
-//         console.log(response.data[0].datetime);
+//         // IMDB Rating
+//         console.log(`IMDB Rating: ${response.data.imdbRating}`);
+
+//         // Rotten Tomatoes
+//         console.log(`Rotten Tomatoes Score: ${response.data.Ratings[1].Value}`);
+
+//         // Country of Production
+//         console.log(`Country of Origin: ${response.data.Country}`);
+
+//         // Movie Language
+//         console.log(`Languages: ${response.data.Language}`);
+
+//         // Plot
+//         console.log(`Plot: ${response.data.Plot}`);
+
+//         // Actors
+//         console.log(`Actors: ${response.data.Actors}`);
+
+
 //     });
 
-    axios.get(`http://www.omdbapi.com/?apikey=7aca9df7&t=batman+begins`).then(function(response, error) {
-        if (error) {
-            return console.log('Error occurred: ' + error);
+var fs = require('fs');
+
+    fs.readFile("random.txt", "utf8", function(error, data){
+        if (error){
+            return console.log(error);
+        };
+        console.log(data);
+        var myData = data.split(',');
+
+        var action = myData[0];
+        
+        console.log(action);
+    
+
+        switch (action){
+            case 'concert-this':{
+                
+                axios.get(`https://rest.bandsintown.com/artists/DVSN/events?app_id=0be905f2241374a45f8824e2ff55e3e7`).then(function( response, error) {
+                    if (error) {
+                        return console.log('Error occurred: ' + error);
+                    }
+                    console.log(response.data[0]);
+                }
+            )
+            break;   
         }
-        /*  * Title of the movie.
-            * Year the movie came out.
-            * IMDB Rating of the movie.
-            * Rotten Tomatoes Rating of the movie.
-            * Country where the movie was produced.
-            * Language of the movie.
-            * Plot of the movie.
-            * Actors in the movie.
-            * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-        */
-        // console.log(response.data);
+        
+        }    
+    });           
+            
+    //         case 'spotify-this-song':
+    //             for(var i = 3; i < results.length; i++){
+    //                 convertedSong.push(results[i]);
+    //             };
+    //             console.log(convertedSong);
+    //             var song = convertedSong.join(" ");
+    //             console.log(song);
+    //             spotify.search({ type: 'track', query: song, limit: 5 }, function(response, error) {
+    //                 if (error) {
+    //                     console.log(error);
+    //                     console.log('Error occurred: ' + error.tracks.items[0]);
+    //                 } 
+    //                 return console.log(response); 
+    //             });
+    //             break;
+            
+    //         case 'movie-this':
+    //            for(var i = 3; i < results.length; i++){
+    //                 convertedMovie.push(results[i]);
+    //             };
+    //                 console.log(convertedMovie);
+    //                 var movie = convertedMovie.join(" ");
+    //                 axios.get(`http://www.omdbapi.com/?apikey=${process.env.OMDB_KEY}&t=${movie}`).then(function(response, error) {
+    //                     if (error) {
+    //                         return console.log('Error occurred: ' + error);
+    //                     }
+    //                     /*  * Title of the movie.
+    //                         * Year the movie came out.
+    //                         * IMDB Rating of the movie.
+    //                         * Rotten Tomatoes Rating of the movie.
+    //                         * Country where the movie was produced.
+    //                         * Language of the movie.
+    //                         * Plot of the movie.
+    //                         * Actors in the movie.
+    //                         * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+    //                     */
+    //                     console.log(response.data);
+    //                 });
+    //             break;
+        
+    //         case 'do-what-it-says':
+    //             fs.readFile("random.txt", "utf8", function(error, data){
+    //                 if (error){
+    //                     return console.log(error);
+    //                 };
+    //                 var myData = data.split(',');
+                
+    //                 for (let i = 0; i < myData.length; i++){
+    //                     console.log(myData[i].trim());
+    //                 }
+    //                 });
+    //                 break;
 
-        // Movie Title
-        console.log(`Movie: ${response.data.Title}`);
+        
 
-        // Release Year
-        console.log(`Released: ${response.data.Year}`);
+    //     // var arr = Array.from(myData);
+    //     // console.log(typeof arr[0]);
 
-        // IMDB Rating
-        console.log(`IMDB Rating: ${response.data.imdbRating}`);
-
-        // Rotten Tomatoes
-        console.log(`Rotten Tomatoes Score: ${response.data.Ratings[1].Value}`);
-
-        // Country of Production
-        console.log(`Country of Origin: ${response.data.Country}`);
-
-        // Movie Language
-        console.log(`Languages: ${response.data.Language}`);
-
-        // Plot
-        console.log(`Plot: ${response.data.Plot}`);
-
-        // Actors
-        console.log(`Actors: ${response.data.Actors}`);
+    //     // // for (var i = 0; i < myData.length; i++){
+    //     // //     randomEntry.push(myData[i]);
+    //     // // };
+    //     // // console.log(typeof randomEntry);
+    //     // // console.log(randomEntry);
+    //     // // console.log(randomEntry[0]);
+    //     // // operation = myData[0];
+    //     // // var title = myData[1];
 
 
-    });
+    //     // console.log(`liri[${arr[0]}](${arr[1]})`);
+    
